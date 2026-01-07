b48bf51e6c7ac016ad819b2f469ba26ce3851301
bigdillyboy <dmostek3@huskers.unl.edu>
2026-01-07 17:09:25 -0600

    Initial Commit

---
+++ RestaurantRequests/RestaurantRequestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantRequestsApp.swift
+//  RestaurantRequests
+//
+//  Created by Dylan Mostek on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantRequestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
